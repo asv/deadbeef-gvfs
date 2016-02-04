d1851abe8c0e54f40027031470eedc6f3b2a729f
Alexey Smirnov <unlinkat@gmail.com>
2016-02-04 13:25:38 +0500

    Added directory support

--- src/deadbeef-gvfs.c
+++ src/deadbeef-gvfs.c
@@ -1,5 +1,8 @@
 #include "config.h"
 
+#include <string.h>
+#include <stdlib.h>
+
 #include <deadbeef/deadbeef.h>
 #include <gio/gio.h>
 
@@ -14,13 +17,11 @@ static DB_functions_t *deadbeef = NULL;
 static DB_vfs_t plugin;
 
 static GVfs *gvfs = NULL;
-static const char *scheme_names[] = { "smb://", "sftp://", "http://", NULL };
+static const char *scheme_names[] = { "smb://", "sftp://", NULL };
 
 static DB_FILE*
 ddb_gvfs_open (const char *path)
 {
-  const char *content_type;
-  g_warning ("Call: %s", __FUNCTION__);
   g_return_val_if_fail (path != NULL, NULL);
 
   GError *error = NULL;
@@ -30,14 +31,12 @@ ddb_gvfs_open (const char *path)
     g_file_query_info (file, G_FILE_ATTRIBUTE_STANDARD_CONTENT_TYPE,
                        G_FILE_QUERY_INFO_NONE, NULL, &error);
 
-  if (info) {
-      content_type = g_strdup (g_file_info_get_content_type (info));
-      g_object_unref (info);
-  } else {
+  if (info == NULL)
+    {
       g_warning ("Error on request file %s information: %s", path, error->message);
       g_error_free (error);
       return NULL;
-  }
+    }
 
   GFileInputStream *handle = g_file_read (file, NULL, &error);
 
@@ -45,24 +44,27 @@ ddb_gvfs_open (const char *path)
   if (handle == NULL)
     {
       g_warning ("Could not open %s for reading: %s", path, error->message);
+
+      g_object_unref (info);
       g_error_free (error);
+
       return NULL;
     }
 
   vfs_gvfs_data_t *data;
+
   data = g_new (vfs_gvfs_data_t, 1);
   data->stream.vfs = &plugin;
-  data->content_type = content_type;
   data->handle = G_INPUT_STREAM (handle);
+  data->content_type = g_strdup (g_file_info_get_content_type (info));
 
-  g_warning ("return from: %s", __FUNCTION__);
+  g_object_unref (info);
   return (DB_FILE *) data;
 }
 
 static void
 ddb_gvfs_close (DB_FILE *stream)
 {
-  g_warning ("Call: %s", __FUNCTION__);
   vfs_gvfs_data_t *data = (vfs_gvfs_data_t *) stream;
   g_return_if_fail (data != NULL);
 
@@ -70,13 +72,11 @@ ddb_gvfs_close (DB_FILE *stream)
     g_object_unref (data->handle);
 
   g_free (data);
-  g_warning ("return from: %s", __FUNCTION__);
 }
 
 static size_t
 ddb_gvfs_read (void *ptr, size_t size, size_t nmemb, DB_FILE *stream)
 {
-/*   g_warning ("Call: %s", __FUNCTION__); */
   vfs_gvfs_data_t *data = (vfs_gvfs_data_t *) stream;
   g_return_val_if_fail (data != NULL, EOF);
   g_return_val_if_fail (! g_input_stream_is_closed (data->handle), -1);
@@ -88,14 +88,13 @@ ddb_gvfs_read (void *ptr, size_t size, size_t nmemb, DB_FILE *stream)
       g_error_free (error);
       return 0;
   }
-/*   g_warning ("return from: %s", __FUNCTION__); */
+
   return bytes / size;
 }
 
 static int
 ddb_gvfs_seek (DB_FILE *stream, int64_t offset, int whence)
 {
-/*   g_warning ("Call: %s", __FUNCTION__); */
   vfs_gvfs_data_t *data = (vfs_gvfs_data_t *) stream;
 
   g_return_val_if_fail (data != NULL, -1);
@@ -120,32 +119,31 @@ ddb_gvfs_seek (DB_FILE *stream, int64_t offset, int whence)
       break;
   }
 
-  GError *err = NULL;
+  GError *error = NULL;
 
-/*   g_warning ("return from: %s", __FUNCTION__); */
-  if (g_seekable_seek (G_SEEKABLE (data->handle), offset, seektype, NULL, &err))
+  if (g_seekable_seek (G_SEEKABLE (data->handle), offset, seektype, NULL, &error))
     return 0;
 
-  g_warning ("Could not seek: %s", err->message);
-  g_error_free (err);
+  g_warning ("Could not seek: %s", error->message);
+  g_error_free (error);
+
   return -1;
 }
 
 static int64_t
 ddb_gvfs_tell (DB_FILE *stream)
 {
-/*   g_warning ("Call: %s", __FUNCTION__); */
   vfs_gvfs_data_t *data = (vfs_gvfs_data_t *) stream;
-  g_return_val_if_fail (data != NULL, -1);
 
-/*   g_warning ("return from: %s", __FUNCTION__); */
+  g_return_val_if_fail (data != NULL, -1);
+  g_return_val_if_fail (! g_input_stream_is_closed (data->handle), -1);
+
   return g_seekable_tell (G_SEEKABLE (data->handle));
 }
 
 static void
 ddb_gvfs_rewind (DB_FILE *stream)
 {
-  g_warning ("Call: %s", __FUNCTION__);
   vfs_gvfs_data_t *data = (vfs_gvfs_data_t *) stream;
   g_return_if_fail (data != NULL);
 
@@ -156,9 +154,10 @@ ddb_gvfs_rewind (DB_FILE *stream)
 static int64_t
 ddb_gvfs_getlength (DB_FILE *stream)
 {
-  g_warning ("Call: %s", __FUNCTION__);
   vfs_gvfs_data_t *data = (vfs_gvfs_data_t *) stream;
+
   g_return_val_if_fail (data != NULL, -1);
+  g_return_val_if_fail (! g_input_stream_is_closed (data->handle), -1);
 
   GError *error = NULL;
   GFileInfo *info;
@@ -168,7 +167,7 @@ ddb_gvfs_getlength (DB_FILE *stream)
                                          NULL, &error);
   if (info == NULL)
     {
-      g_warning ("getlength(): error: %s", error->message);
+      g_warning ("Could not read stream info: %s", error->message);
       g_error_free (error);
       return -1;
     }
@@ -176,22 +175,78 @@ ddb_gvfs_getlength (DB_FILE *stream)
   gint64 size = g_file_info_get_attribute_uint64 (info, G_FILE_ATTRIBUTE_STANDARD_SIZE);
   g_object_unref (info);
 
-  g_warning ("return from: %s, ret=%d", __FUNCTION__, size);
   return size;
 }
 
 int
-ddb_gvfs_scandir (const char *dir, struct dirent ***namelist,
-                  int (*selector) (const struct dirent *), int (*cmp) (const struct dirent **, const struct dirent **))
+ddb_gvfs_scandir (const char *dir, struct dirent ***namelist, int (*selector) (const struct dirent *), int (*cmp) (const struct dirent **, const struct dirent **))
 {
-  g_warning ("ddb_gvfs scandir(): not implemented yet");
-  return -1;
+  GQueue *file_list = g_queue_new ();
+  GQueue *dir_list = g_queue_new ();
+  g_queue_push_head (dir_list, g_file_new_for_uri (dir));
+
+  GFile *gdir;
+  while ((gdir = g_queue_pop_head (dir_list)) != NULL)
+    {
+      GFileEnumerator *file_enumerator = g_file_enumerate_children (gdir, G_FILE_ATTRIBUTE_STANDARD_NAME, G_FILE_QUERY_INFO_NOFOLLOW_SYMLINKS, NULL, NULL);
+      if (file_enumerator == NULL)
+        {
+          g_object_unref (gdir);
+          continue;
+        }
+
+      GFileInfo *file_info;
+      GFile *child_file;
+
+     while ((file_info = g_file_enumerator_next_file (file_enumerator, NULL, NULL)) != NULL)
+        {
+          child_file = g_file_get_child (gdir, g_file_info_get_name (file_info));
+          g_object_unref (file_info);
+
+          if (g_file_query_file_type (child_file, G_FILE_QUERY_INFO_NONE, NULL) == G_FILE_TYPE_DIRECTORY)
+            g_queue_push_head (dir_list, child_file);
+          else
+            {
+              g_queue_push_tail (file_list, g_file_get_uri (child_file));
+              g_object_unref (child_file);
+            }
+        }
+
+      g_file_enumerator_close (file_enumerator, NULL, NULL);
+      g_object_unref (file_enumerator);
+
+      g_object_unref (gdir);
+    }
+
+  g_queue_free (dir_list);
+
+  int num_files = 0;
+  *namelist = malloc (sizeof(void *) * g_queue_get_length (file_list));
+
+  char *fname;
+  while ((fname = g_queue_pop_head (file_list)) != NULL)
+    {
+      struct dirent entry;
+      strncpy (entry.d_name, fname, sizeof(entry.d_name) - 1);
+      entry.d_name[sizeof(entry.d_name) - 1] = '\0';
+
+      if (selector == NULL || (selector && selector(&entry)))
+        {
+          (*namelist)[num_files] = calloc (1, sizeof (struct dirent));
+          strcpy ((*namelist)[num_files]->d_name, entry.d_name);
+          num_files++;
+        }
+
+      g_free (fname);
+    }
+
+  g_queue_free (file_list);
+  return num_files;
 }
 
 const char **
 ddb_gvfs_get_schemes (void)
 {
-  /* TODO: use g_vfs_get_supported_uri_schemes */
   return scheme_names;
 }
 
@@ -202,9 +257,24 @@ ddb_gvfs_is_streaming (void)
 }
 
 int
-ddb_gvfs_is_container (const char *path)
+ddb_gvfs_is_container (const char *fname)
 {
-  return 0;
+  const char **sn = scheme_names;
+  while (*sn)
+    {
+      if (g_str_has_prefix (fname, *sn))
+        break;
+      sn++;
+    }
+
+  if (*sn == NULL)
+    return 0;
+
+  GFile *file = g_file_new_for_uri (fname);
+  GFileType type = g_file_query_file_type (file, G_FILE_QUERY_INFO_NONE, NULL);
+  g_object_unref (file);
+
+  return type == G_FILE_TYPE_DIRECTORY;
 }
 
 static int
@@ -258,7 +328,7 @@ static DB_vfs_t plugin = {
     .plugin.name = "GVFS",
     .plugin.descr = "GVFS support",
     .plugin.copyright = "Public Domain",
-    .plugin.website = "example.com",
+    .plugin.website = "https://github.com/asv/deadbeef-gvfs",
 
     .plugin.start = ddb_gvfs_start,
     .plugin.stop = ddb_gvfs_stop,
@@ -281,6 +351,7 @@ static DB_vfs_t plugin = {
 
     .is_streaming = ddb_gvfs_is_streaming,
     .is_container = ddb_gvfs_is_container,
+
     .scandir = ddb_gvfs_scandir
 };
 
